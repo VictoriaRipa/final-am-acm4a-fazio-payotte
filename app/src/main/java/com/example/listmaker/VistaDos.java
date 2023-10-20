7163e96cd7f0ab2e7b8fceb785f784766e292498
santino fazio <115169486+SantinoFazio@users.noreply.github.com>
2023-10-20 19:46:14 -0300

    add text
    contiene lo proximo a realizar en la vista 2

--- app/src/main/java/com/example/listmaker/VistaDos.java
+++ app/src/main/java/com/example/listmaker/VistaDos.java
@@ -3,13 +3,27 @@ package com.example.listmaker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class VistaDos extends AppCompatActivity {
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vista_dos);
+
+
     }
+
+
 }
