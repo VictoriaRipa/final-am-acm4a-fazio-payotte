bfdab14d8015a83d32db26dd313ded06d7349760
santino fazio <115169486+SantinoFazio@users.noreply.github.com>
2023-10-20 18:25:54 -0300

    end primer vista

--- app/src/main/java/com/example/listmaker/MainActivity.java
+++ app/src/main/java/com/example/listmaker/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.listmaker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,4 +14,13 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    public void showToast(View view) {
+        String msg = "Hello Toast!";
+        Toast toast = Toast.makeText(
+                this, msg, Toast.LENGTH_LONG);
+        toast.show();
+        TextView textView = findViewById(R.id.botonIngresar);
+        textView.setText("ingresando..");
+    }
+
 }
