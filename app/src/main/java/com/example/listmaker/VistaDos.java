c5409c8718a1bffa67d86e4f1c9b5f1bf87ceef5
VictoriaRipa <maria.payotte@davinci.edu.ar>
2023-10-21 16:58:41 -0300

    add text and layout
    agregue una segunda vista, en la cual se pueden ingresar las tareas y al hacer click eliminarlas.

--- app/src/main/java/com/example/listmaker/VistaDos.java
+++ app/src/main/java/com/example/listmaker/VistaDos.java
@@ -1,29 +1,54 @@
 package com.example.listmaker;
-
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
-import android.view.View;
 import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.TextView;
-
+import android.widget.Button;
 import java.util.ArrayList;
 import java.util.List;
 
-public class VistaDos extends AppCompatActivity {
+    public class VistaDos extends AppCompatActivity {
+        private List<String> tasks;
+        private ArrayAdapter<String> taskAdapter;
+        private EditText taskEditText;
+        private ListView taskListView;
 
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_vista_dos);
 
+            tasks = new ArrayList<>();
+            taskAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, tasks);
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_vista_dos);
+            taskListView = findViewById(R.id.taskListView);
+            taskListView.setAdapter(taskAdapter);
 
+            taskEditText = findViewById(R.id.taskEditText);
+            Button addButton = findViewById(R.id.addButton);
 
+            addButton.setOnClickListener(view -> addTask());
+
+            // Esto cuando haces click elimina la tarea
+            taskListView.setOnItemClickListener((parent, view, position, id) -> deleteTask(position));
+        }
+
+        private void addTask() {
+            String taskText = taskEditText.getText().toString();
+
+            if (!taskText.isEmpty()) {
+                // Agrega la tarea
+                taskAdapter.add(taskText);
+
+                // Borra el texto
+                taskEditText.setText("");
+            }
+        }
+
+        private void deleteTask(int position) {
+            // Elimina la tarea seleccionada
+            taskAdapter.remove(taskAdapter.getItem(position));
+        }
     }
 
-
-}
