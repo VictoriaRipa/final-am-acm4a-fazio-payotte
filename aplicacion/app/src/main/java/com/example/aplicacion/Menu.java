d7bc275a46844b6ae5b961478f4c38ee984089f0
VictoriaRipa <maria.payotte@davinci.edu.ar>
2023-12-16 19:11:17 -0300

    feat: add 'Cerrar Sesión' button

--- aplicacion/app/src/main/java/com/example/aplicacion/Menu.java
+++ aplicacion/app/src/main/java/com/example/aplicacion/Menu.java
@@ -1,10 +1,12 @@
 package com.example.aplicacion;
-
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class Menu extends AppCompatActivity {
 
@@ -12,20 +14,36 @@ public class Menu extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
+
+        Button cerrarSesionButton = findViewById(R.id.buttonCerrarSesion);
+        cerrarSesionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cerrarSesion();
+            }
+        });
     }
 
+    private void cerrarSesion() {
+        FirebaseAuth.getInstance().signOut();
+        Intent intent = new Intent(this, Login.class);
+        startActivity(intent);
+        finish(); // Cierra la actividad actual
+    }
 
     public void ingresando(View view){
-        Intent ingresarAPrimerVista =new Intent(this,VistaDos.class);
+        Intent ingresarAPrimerVista = new Intent(this, VistaDos.class);
         startActivity(ingresarAPrimerVista);
     }
 
     public void completadas(View view){
-        Intent completadas =new Intent(this,Completadas.class);
+        Intent completadas = new Intent(this, Completadas.class);
         startActivity(completadas);
     }
+
     public void Imagen(View view){
-        Intent img =new Intent(this, Imagen.class);
+        Intent img = new Intent(this, Imagen.class);
         startActivity(img);
     }
 }
+
