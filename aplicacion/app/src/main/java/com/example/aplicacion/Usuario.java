45d2170054c5374716d4d3d9472c2afc0f3ac712
VictoriaRipa <maria.payotte@davinci.edu.ar>
2023-12-17 14:12:12 -0300

    Update Usuario.java

--- aplicacion/app/src/main/java/com/example/aplicacion/Usuario.java
+++ aplicacion/app/src/main/java/com/example/aplicacion/Usuario.java
@@ -2,7 +2,7 @@ package com.example.aplicacion;
 public class Usuario {
     private String nombre;
     private String email;
-    // Otros campos según tus necesidades
+    
 
     public Usuario() {
         // Constructor vacío requerido por Firebase Realtime Database
@@ -11,7 +11,7 @@ public class Usuario {
     public Usuario(String nombre, String email /* otros campos */) {
         this.nombre = nombre;
         this.email = email;
-        // Inicializar otros campos según tus necesidades
+
     }
 
     // Métodos getters y setters para los campos del usuario
@@ -31,5 +31,4 @@ public class Usuario {
         this.email = email;
     }
 
-    // Agrega getters y setters para otros campos si es necesario
 }
